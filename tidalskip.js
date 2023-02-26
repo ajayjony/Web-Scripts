438d1aaad17a2dcf218b60918bdfd76f41a2488d
Ajay Kumar <63363721+ajayjony@users.noreply.github.com>
2023-02-26 13:22:40 +0530

    Create tidalskip.js

---
+++ tidalskip.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+         function myFunction() {
+            var min = 35,
+               max = 75;
+            var rand = Math.floor(Math.random() * (max - min + 1) + min); //Generate Random number between min-max
+            for (const button of document.querySelectorAll('button[data-type="button__skip-next"]')) {
+               button.click();
+            }
+            setTimeout(myFunction, rand * 1000);
+         }
+
+         myFunction()
+      
